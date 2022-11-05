1b8daddc739224b686557294b8187a9a6a6127d8
jiyong <wlfyd0419@gmail.com>
2022-11-05 15:34:22 +0900

    Finished Movie App2

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/movie/:id" element={<Detail />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/user" element={<h1>user</h1>} />
+        <Route path={process.env.PUBLIC_URL + "/movie/:id"} element={<Detail />} />
+        <Route path={process.env.PUBLIC_URL + "/user"} element={<h1>user</h1>} />
+        <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
       </Routes>
     </Router>
   );
@@ -19,6 +19,7 @@ function App() {
 <Routes> => Routes는 Routes안에 작성된 Route들에게 접근 <ul>같은 느낌이라고 보자
 <Route>  => Routes 안에 작성되며 <li>같은 느낌이라고 보자
             Route path 는 route(경로)를 적어주고, element 에는 렌더링할 html태그 또는 component를 적어주자
+            process.env.PUBLIC_URL + 는 gh-pages에 작업물의 Route를 등록하기위해 쓴것
 
 */
 export default App;
